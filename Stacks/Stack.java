ee72ca18093a272b0ff9c47399f2517e7be35362
Chhaya <tyagi.chhaya4@gmail.com>
2025-06-11 20:11:42 +0530

    mystack

--- Stacks/Stack.java
+++ Stacks/Stack.java
@@ -1,48 +1,52 @@
 
 public class Stack {
-   private int[] arr=new int[10];
-   int top;
-   int size;
+  private int[] arr=new int[20];
+  int top;
+  int size;
 
-   public Stack(){
-     size=arr.length;
-     top=-1;
-   }
-    
-   public void push(int data){
-    if(top<size-1)
-    {
+  public Stack(){
+   top=-1;
+   size=arr.length; 
+  }
+  
+  public void push(int data)
+  {
+     if(top<size-1){
         arr[++top]=data;
-    }
-    else{
-     System.out.println("Stack Overflow");
-    }
-   }
-   
-   public int pop(){
-    if(top>-1){
-    return arr[top--];
-    }
-    else{
-        System.out.println("Stack underflow");
-        return 0;
-    }
-   }
+     }else{
+        System.out.println("Stack Overflow");
+     }
+  }
 
-   public int peek(){
-    if(top>-1){
-    return arr[top];}
-    else{
-        System.out.println("Stack is empty");
-        return 0;
-    }
-   }
+  public int pop(){
+     if(top>-1){
+        return arr[top--];
+     }
+        else 
+        {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+     }
 
-   public void printStack(){
+  public int peek()
+{  
+    if(top>-1){
+    return arr[top];
+    }
+    else {
+    System.out.println("Stack is Empty");
+    return 0;
+    }
+}
+
+public void printStack(){
     for(int i=0;i<=top;i++){
         System.out.print(arr[i]+" ");
     }
     System.out.println();
-   }
+}
 
 }
+
+
